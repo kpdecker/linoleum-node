a12d61f38f7b0c3797f00edc318c55a126d11eea
kpdecker <kpdecker@gmail.com>
2016-01-07 23:39:00 -0600

    Limit babel compilation to changed files

--- tasks/babel.js
+++ tasks/babel.js
@@ -1,5 +1,6 @@
 import Gulp from 'gulp';
 import babel from 'gulp-babel';
+import changed from 'gulp-changed';
 import plumber from '@kpdecker/linoleum/src/plumber';
 
 import {SOURCE_FILES, BUILD_TARGET} from '@kpdecker/linoleum/config';
@@ -8,6 +9,7 @@ import BABEL_DEFAULTS from '@kpdecker/linoleum/babel-defaults';
 Gulp.task('babel', function() {
   return Gulp.src(SOURCE_FILES)
       .pipe(plumber())
+      .pipe(changed(BUILD_TARGET))
       .pipe(babel(BABEL_DEFAULTS))
       .pipe(Gulp.dest(BUILD_TARGET));
 });
