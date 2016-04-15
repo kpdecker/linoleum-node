236a99bd6a010870061e6469c0973e6611dd830c
kpdecker <kpdecker@gmail.com>
2016-04-15 12:17:53 -0500

    Add mocha timeout test option

--- tasks/cover.js
+++ tasks/cover.js
@@ -5,7 +5,7 @@ import mocha from 'gulp-mocha';
 import {instrumenterConfig} from '@kpdecker/linoleum/src/cover';
 import plumber from '@kpdecker/linoleum/src/plumber';
 
-import {SOURCE_FILES, COVERAGE_TARGET, testFiles} from '@kpdecker/linoleum/config';
+import {SOURCE_FILES, COVERAGE_TARGET, MOCHA_TIMEOUT, testFiles} from '@kpdecker/linoleum/config';
 
 function coverSourceFiles() {
   return Gulp.src(SOURCE_FILES)
@@ -22,7 +22,7 @@ Gulp.task('cover:mocha:pre', function() {
 Gulp.task('cover:mocha:run', ['cover:mocha:pre'], function() {
   return Gulp.src(testFiles())
       .pipe(plumber())
-      .pipe(mocha());
+      .pipe(mocha({timeout: MOCHA_TIMEOUT || 2000}));
 });
 Gulp.task('cover:mocha', ['cover:mocha:run'], function() {
   return Gulp.src(testFiles())
